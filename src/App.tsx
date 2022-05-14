7a1d727d1eee70a35542e65c56dde09464d93312
Masaki Mita <mimimita3131@gmail.com>
2022-05-15 02:09:13 +0900

    バージョン表記の追加

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import {BrowserRouter, Navigate, Route, Routes, useNavigate, useParams} from "react-router-dom";
 import {Home} from "./pages/Home";
 import {Result} from "./pages/Result";
+import {VERSION} from "./version";
 
 export const App: React.FC = () => {
     const getRandomSeed = () => Math.floor(Math.random() * 1000000000);
@@ -14,7 +15,7 @@ export const App: React.FC = () => {
         return isNaN(seed) ? <Navigate to="/" replace/> : <Game seed={seed}/>;
     };
     return (
-        <Container m="auto" maxW="500pt" alignItems="center">
+        <Container m="auto" maxW="500pt" alignItems="center" h="100vh">
             <Flex position="absolute" bgColor="lightgray" minW="500pt" h="100vh" m="0" p="0" opacity={0.1} zIndex={-1}/>
             <Heading padding="5pt" marginY="5pt" display="flex">
                 <Flex onClick={() => window.location.href = "/"} cursor="pointer" width="auto">🌎</Flex>
@@ -34,6 +35,7 @@ export const App: React.FC = () => {
                     </Routes>
                 </BrowserRouter>
             </Box>
+            <Box position="absolute" left={0} bottom={0} m="5pt">ver. {VERSION}</Box>
         </Container>
     );
 }
